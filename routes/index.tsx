b5f1698a654d3779ca7ba1ebf13b2f94b0e7b0ec
Nap Joseph Calub <njncalub@gmail.com>
2026-05-27 14:31:09 +0800

    refactor: remove unused context parameter from Home function

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 import { Head } from "fresh/runtime";
 import { define } from "../utils.ts";
 
-export default define.page(function Home(ctx) {
+export default define.page(function Home() {
   return (
     <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
       <Head>
